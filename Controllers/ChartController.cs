6e2d33f7bd6e2575ffe4a26e9a6a492b4d01e1a9
Mike Sifanele <masilive.sifanele@gmail.com>
2022-12-13 22:29:29 +0200

    Initial commit

---
+++ Controllers/ChartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mayande.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
