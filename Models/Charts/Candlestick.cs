1b7ce499b3f11f87047115d66b9d800f8a00be99
Mike Sifanele <masilive.sifanele@gmail.com>
2022-12-20 23:29:07 +0200

    added candlesticks model

---
+++ Models/Charts/Candlestick.cs
@@ -0,0 +1,26 @@
+﻿namespace Mayande.Models.Charts
+{
+    public struct Candlestick
+    {
+        public DateTime Timestamp { get; set; }
+        public float Open { get; set; }
+        public float High { get; set; }
+        public float Low { get; set; }
+        public float Close { get; set; }
+        public float Volume { get; set; }
+        public string Points { get; set; }
+        public string GetCandleColor()
+        {
+            return Open > Close ? "red" : "lime";
+        }
+    }
+    public struct Candlesticks
+    {
+        public static Candlestick[] GetRollingWindow()
+        {
+            return new Candlestick[] {
+                new Candlestick() { Points = "50,50 30,70 50,100 70,70 50,50" }
+            };
+        }
+    }
+}
