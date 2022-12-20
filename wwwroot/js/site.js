d5bf1e124fc8f73d1f3eef65cb8f642b0b5149ff
Mike Sifanele <masilive.sifanele@gmail.com>
2022-12-20 21:20:58 +0200

    added cool height to chart

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function debounce(func, wait) {
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+}
+
+let chartHeight = '0';
+const setChartDimensions = debounce(() => {
+    chartHeight = window.innerHeight - (document.getElementById('header').getBoundingClientRect().height + document.getElementById('footer').getBoundingClientRect().height);
+
+    if (document.getElementById('chart').style.height !== chartHeight) {
+        document.getElementById('chart').setAttribute('height', chartHeight);
+    }
+}, 750);
+
+window.onresize = () => {
+    setChartDimensions();
+}
+window.onload = () => {
+    setChartDimensions();
+}
